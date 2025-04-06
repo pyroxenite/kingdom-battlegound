0dbb896f9c8e29405221f9eba813aa65d2136cf2
Neonix13 <130598206+Neonix13@users.noreply.github.com>
2025-04-07 00:12:33 +0200

    discussion

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
 console.log("Kingdom Battleground Loaded")
 ui.notifications.info("Kingdom Battleground Loaded");
+
+function add_listen_socket() {
+    game.socket.on('kingdom.com', (arg1, arg2, arg3) => {
+        console.log(arg1, arg2, arg3); // expected: "foo bar bat"
+      })
+}
+
+function talk() {
+    const targetUser = game.users.find(u => u.name === "Assistant MJ");
+
+if (!targetUser) {
+  ui.notifications.warn("Utilisateur non trouvé !");
+  return;
+}
+
+game.socket.emit("kingdom.com", {
+  userId: targetUser.name,
+  title: "Un message secret",
+  content: "<p>Voici une surprise pour toi !</p>"
+});
+}
