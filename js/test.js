29a92210848f0c53df35cc7b93d077032f4e07ba
pyroxenite <48839145+pyroxenite@users.noreply.github.com>
2025-04-07 01:59:10 +0200

    display choice v2

--- js/test.js
+++ js/test.js
@@ -1,22 +1,16 @@
 kingdom = {}
-kingdom.callbacks = {}
-
-
-
+//kingdom.callbacks = {}
 
 function display_choice(prompt, options) {
     let choice_message_id = crypto.randomUUID();
 
-    kingdom.callbacks[choice_message_id] = {}
-    for (let option_id in options) {
-        let option = options[option_id];
-        kingdom.callbacks[choice_message_id][option_id] = option.callback;
-    }
-
+    //kingdom.callbacks[choice_message_id] = {}
     let options_html = ""
     for (let option_id in options) {
         let option = options[option_id];
-        options_html += `<div class="choice" style="--color: ${option.color};" onclick="kingdom.callbacks['${choice_message_id}']['${option_id}']()">${option.name}</div>`
+        //kingdom.callbacks[choice_message_id][option_id] = option.callback;
+        options_html += `<div class="choice" style="--color: ${option.color};" id="${choice_message_id}_${option_id}">${option.name}</div>`
+
     }
 
     ChatMessage.create({
@@ -27,5 +21,12 @@ function display_choice(prompt, options) {
         ${options_html}
       </div>
         `
+    }).then(() => {
+        for (let option_id in options) {
+            let option = options[option_id];
+            let element_id = `${choice_message_id}_${option_id}`;
+            document.querySelector(".choice#" + element_id).addEventListener("click", () => {
+                option.callback();
+            })
     });
 }
